23f07831ac7cf9b68b47c7af520a055f4f16dfd9
Podolianyk <podolianik1988@gmail.com>
2024-03-03 16:18:55 +0100

    task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,34 +1,27 @@
-// *Задача 1. Акаунт користувача
-// Перед звільненням розробник зламав вихідний код управління акаунтами користувачів нашого сервісу доставки їжі. 
-// Виконай рефакторинг методів об'єкта customer, розставивши відсутні this під час звернення до властивостей об'єкта.
+const allList = document.querySelectorAll(".item") // отримую доступ до всіх li з класом item, повертає масив посилань
 
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+// console.log(allList);
+console.log(`Number of categories: ${allList.length}`); 
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+const allTitle = document.querySelectorAll("h2") // отримую доступ до всіх h2, повертає масив посилань
+// console.log(allTitle);
+allTitle.forEach(function (title) {
+    const titleName = title.textContent
+    console.log(`Category: ${titleName}`);
+    const categoriesNames = title.parentElement.querySelectorAll("ul > li");
+    console.log(`Elements: ${categoriesNames.length}`);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
